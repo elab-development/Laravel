971d62560dc4eac1257b7081bfdb1746a34c96b8
ziksona1312 <102611527+ziksona1312@users.noreply.github.com>
2024-01-20 16:48:11 +0100

    Movie model

---
+++ database/seeders/MoviesTableSeeder.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Movie;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class MoviesTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        Movie::factory(13)->create();
+    }
+}
